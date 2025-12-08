1cd5aa4df93a07212e274623a56cbca48c44cbb9
Zack Yuan <zack6yuan@gmail.com>
2025-12-08 16:31:37 -0600

    trying to get fonts to load.

--- diinker-web/src/components/NavBar.tsx
+++ diinker-web/src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 export default function Navbar() {
   return (
-    <div className="text-white flex flex-row justify-between bg-[#0000007c] h-20 rounded-b-2xl items-center px-10">
+    <div className="text-white flex flex-row justify-between bg-[#0000007c] h-20 rounded-b-2xl items-center px-10 font-nature">
       <div className="text-3xl font-bold">
-        <p>Diinker</p>
+        <p className="">Diinker</p>
       </div>
       <div className="text-lg flex flex-row gap-8">
-        <p>Download</p>
+        <p className="">Download</p>
         <p>About</p>
         <p>Contact</p>
       </div>
